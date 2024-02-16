ace65f63958af1455028a906094c0febdc50bae0
Prashant Rewar <108176843+prashantrewar@users.noreply.github.com>
2024-02-16 23:26:38 +0530

    Add test logs for LFX mentorship task
    Signed-off-by: Prashant Rewar <108176843+prashantrewar@users.noreply.github.com>

--- cmd/antrea-controller/controller.go
+++ cmd/antrea-controller/controller.go
@@ -121,6 +121,8 @@ var allowedPaths = []string{
 
 // run starts Antrea Controller with the given options and waits for termination signal.
 func run(o *Options) error {
+	klog.InfoS("Test log", "Github Username", "prashantrewar", "Pod name", env.GetPodName())
+
 	klog.Infof("Starting Antrea Controller (version %s)", version.GetFullVersion())
 	// Create K8s Clientset, Aggregator Clientset, CRD Clientset and SharedInformerFactory for the given config.
 	// Aggregator Clientset is used to update the CABundle of the APIServices backed by antrea-controller so that
